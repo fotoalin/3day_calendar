b2f0c66dc8cc1fb5a015b3f5e7f4e810e0b5b325
Alin Morosanu <fotoalin@users.noreply.github.com>
2025-01-13 12:31:03 +0000

    refactor: Simplify date formatting and improve button conditionals in calendar template

--- custom_filters.py
+++ custom_filters.py
@@ -1,18 +1,17 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 def format_date(date_string):
     """Convert a date string to a different format."""
-
-    if date_string is None:
-        return ""
-
-    date = datetime.strptime(date_string, "%Y-%m-%d")
+    try:
+        date = datetime.strptime(date_string, '%Y-%m-%d')
+    except ValueError:
+        return date_string  # Return the original string if it doesn't match the format
 
     # Define the suffixes for the days of the month
     if 4 <= date.day <= 20 or 24 <= date.day <= 30:
-        suffix = "th"
+        suffix = 'th'
     else:
-        suffix = ["st", "nd", "rd"][date.day % 10 - 1]
+        suffix = ['st', 'nd', 'rd'][date.day % 10 - 1]
 
-    return date.strftime(f"%B {date.day}{suffix}")
+    return date.strftime(f'%-d{suffix} %B')
